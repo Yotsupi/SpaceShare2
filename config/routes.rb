78c8bccf7928a8eb9a2176135e65d723e6986f89
Yotsupi <kayoshida.1830@gmail.com>
2021-11-01 08:00:16 +0000

    [Add]model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :hosts
+  devise_for :hosts, controller{
+    
+  }
   devise_for :guests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: ''
+  
 end
