58064b93395b22e8abfb1a39fc72c95f62dfbf0e
Yotsupi <kayoshida.1830@gmail.com>
2021-11-01 11:53:55 +0000

    [Add]Bootstrap

--- app/controllers/host/spaces_controller.rb
+++ app/controllers/host/spaces_controller.rb
@@ -2,10 +2,22 @@ class Host::SpacesController < ApplicationController
   def new
     @space = Space.new
   end
-  
-  private
-  def space_params
-    
+
+  def create
+    @space = Space.new(space_params)
+    @space.host_id = current_host.id
+    @space.save
+    redirect_to root_path
   end
   
+  def show
+    @space = Space.find(params[:id])
+  end
+
+
+  private
+  def space_params
+    params.require(:space).permit(:name, :address, :detail, :capacity, :open_time, :close_time, :hourly_rate, :image)
+  end
+
 end
