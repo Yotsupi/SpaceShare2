7cf429c86c16146b6fe2303ec9ff3b81b27d2d8a
Yotsupi <kayoshida.1830@gmail.com>
2021-11-20 12:31:59 +0000

    [Fix]validation

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,6 +2,7 @@ class Space < ApplicationRecord
   has_many :reservations, dependent: :destroy
   belongs_to :host
   attachment :image
+  validate :start_finish_check
 
   with_options presence: true do
     validates :name
@@ -10,6 +11,12 @@ class Space < ApplicationRecord
     validates :image_id
     validates :capacity
     validates :hourly_rate
+  end
+
+  def start_finish_check
+    if self.open_time >= self.close_time
+      errors.add(:close_time, "利用可能終了時刻は利用可能開始時刻より遅い時間を選択してください")
+    end
   end
 
 end
