62fa168f45522a05696812b4101df9028b96328a
Yotsupi <kayoshida.1830@gmail.com>
2021-11-20 08:12:30 +0000

    [Fix]mypage

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -9,8 +9,14 @@ class GuestsController < ApplicationController
   
   def update
     guest = current_guest
-    guest.update
+    guest.update(guest_params)
     redirect_to 
   end
+  
+  def guest_params
+    params.require(:guest).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
+  end
+  
+  
   
 end
